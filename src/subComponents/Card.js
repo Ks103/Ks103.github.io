32bf458cd37eff9121ec95eae09ad186eff02262
Gaurav Kedia <kediagaurav1709@gmail.com>
2026-01-22 23:31:13 +0530

    Linting Fix

--- src/subComponents/Card.js
+++ src/subComponents/Card.js
@@ -1,8 +1,6 @@
 import { motion } from 'framer-motion';
-import React from 'react'
-import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-import { Behance, Figma } from '../components/AllSvgs';
+import { Figma } from '../components/AllSvgs';
 
 
 
